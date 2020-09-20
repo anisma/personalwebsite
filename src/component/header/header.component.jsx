acf0c795bad190beb0554147cd3814bc076638b0
anisma <anismaghfiroh63@gmail.com>
2020-09-20 15:46:57 +0700

    ok

--- src/component/header/header.component.jsx
+++ src/component/header/header.component.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import { useSpring, animated } from 'react-spring';
 import { Link } from 'react-router-dom';
 import PageContext from '../../context/PageContext';
